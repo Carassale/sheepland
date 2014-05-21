48a0f56ac7fa9aed88915f9f851c9ce9d2b6626c
Gabriele <lele9.2@hotmail.it>
2014-05-21 11:24:51 +0200

    rimossi test vuoti

---
+++ francesco1.corsini-gabriele.carassale/src/test/java/it/polimi/deib/provaFinale2014/francesco1/corsini_gabriele/carassale/connection/ConnectionManagerSocketTest.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.deib.provaFinale2014.francesco1.corsini_gabriele.carassale.connection;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lelino
+ */
+public class ConnectionManagerSocketTest {
+
+    public ConnectionManagerSocketTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /*    
+     @Test
+     public void testStartThread() {
+     System.out.println("startThread");
+     ConnectionManagerSocket instance = null;
+     instance.startThread();
+     fail("The test case is a prototype.");
+     }
+
+
+     @Test
+     public void testStartAction() {
+     System.out.println("startAction");
+     ConnectionManagerSocket instance = null;
+     instance.startAction();
+     fail("The test case is a prototype.");
+     }
+
+     @Test
+     public void testDoAction() {
+     System.out.println("doAction");
+     ConnectionManagerSocket instance = null;
+     boolean expResult = false;
+     boolean result = instance.doAction();
+     assertEquals(expResult, result);
+     fail("The test case is a prototype.");
+     }
+
+     @Test
+     public void testGetPlayerConnections() {
+     System.out.println("getPlayerConnections");
+     ConnectionManagerSocket instance = null;
+     ArrayList<PlayerConnectionSocket> expResult = null;
+     ArrayList<PlayerConnectionSocket> result = instance.getPlayerConnections();
+     assertEquals(expResult, result);
+     fail("The test case is a prototype.");
+     }
+
+     @Test
+     public void testWakeUpPlayer() {
+     System.out.println("wakeUpPlayer");
+     PlayerConnectionSocket pcs = null;
+     ConnectionManagerSocket instance = null;
+     instance.wakeUpPlayer(pcs);
+     fail("The test case is a prototype.");
+     }
+
+     @Test
+     public void testRefreshGame4AllPlayer() {
+     System.out.println("refreshGame4AllPlayer");
+     ConnectionManagerSocket instance = null;
+     instance.refreshGame4AllPlayer();
+     fail("The test case is a prototype.");
+     }
+
+     @Test
+     public void testSetNickName() {
+     System.out.println("setNickName");
+     ConnectionManagerSocket instance = null;
+     instance.setNickName();
+     fail("The test case is a prototype.");
+     }
+
+
+     @Test
+     public void testNextPlayerConnections() {
+     System.out.println("nextPlayerConnections");
+     ConnectionManagerSocket instance = null;
+     instance.nextPlayerConnections();
+     fail("The test case is a prototype.");
+     }
+
+     @Test
+     public void testMoveShepard() throws Exception {
+     System.out.println("moveShepard");
+     ConnectionManagerSocket instance = null;
+     boolean expResult = false;
+     boolean result = instance.moveShepard();
+     assertEquals(expResult, result);
+     fail("The test case is a prototype.");
+     }
+
+
+     @Test
+     public void testMoveSheep() throws Exception {
+     System.out.println("moveSheep");
+     ConnectionManagerSocket instance = null;
+     boolean expResult = false;
+     boolean result = instance.moveSheep();
+     assertEquals(expResult, result);
+     fail("The test case is a prototype.");
+     }
+
+     @Test
+     public void testBuyCard() throws Exception {
+     System.out.println("buyCard");
+     ConnectionManagerSocket instance = null;
+     boolean expResult = false;
+     boolean result = instance.buyCard();
+     assertEquals(expResult, result);
+     fail("The test case is a prototype.");
+     }
+     @Test
+     public void testKillSheep() throws Exception {
+     System.out.println("killSheep");
+     ConnectionManagerSocket instance = null;
+     boolean expResult = false;
+     boolean result = instance.killSheep();
+     assertEquals(expResult, result);
+     fail("The test case is a prototype.");
+     }
+     @Test
+     public void testJoinSheep() throws Exception {
+     System.out.println("joinSheep");
+     ConnectionManagerSocket instance = null;
+     boolean expResult = false;
+     boolean result = instance.joinSheep();
+     assertEquals(expResult, result);
+     fail("The test case is a prototype.");
+     }
+     */
+}
--- francesco1.corsini-gabriele.carassale/src/test/java/it/polimi/deib/provaFinale2014/francesco1/corsini_gabriele/carassale/controller/GameControllerTest.java
+++ francesco1.corsini-gabriele.carassale/src/test/java/it/polimi/deib/provaFinale2014/francesco1/corsini_gabriele/carassale/controller/GameControllerTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package it.polimi.deib.provaFinale2014.francesco1.corsini_gabriele.carassale.controller;
 
 import org.junit.Before;
@@ -14,42 +13,41 @@ import org.junit.Test;
  * @author Francesco Corsini
  */
 public class GameControllerTest {
-    
+
     private PlayerPool playerPool;
     private GameController gameController;
-    
+
     @Before
     public void setUp() {
         gameController = new GameController();
         playerPool = new PlayerPool();
     }
 
-    @Test
-    public void testDistributeCard() {
-        
-    }
     /*
-    @Test
-    public void testPlayRounds() {
+     @Test
+     public void testDistributeCard() {
+
+     }
+     @Test
+     public void testPlayRounds() {
         
-    }
+     }
 
 
-    @Test
-    public void testGetPlayerPool() {
+     @Test
+     public void testGetPlayerPool() {
         
-    }
+     }
 
 
-    @Test
-    public void testPlayGame() {
+     @Test
+     public void testPlayGame() {
         
-    }
+     }
 
 
-    @Test
-    public void testGetGameTable() {
+     @Test
+     public void testGetGameTable() {
         
-    }*/
-    
+     }*/
 }
